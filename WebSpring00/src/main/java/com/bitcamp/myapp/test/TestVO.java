08966b2b85a7e20ed0e11c3fffbbbf4b8465828b
seoyoon-kim <76926158+seoyoon-kim@users.noreply.github.com>
2021-04-05 22:15:00 +0900

    04/05

---
+++ WebSpring00/src/main/java/com/bitcamp/myapp/test/TestVO.java
@@ -0,0 +1,27 @@
+package com.bitcamp.myapp.test;
+
+public class TestVO {
+	private String username;
+	private int age; 
+	private String msg;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	
+}
---
+++ WebSpring00/src/main/webapp/js_css/script.js
@@ -0,0 +1,1 @@
+document.write("js 파일에서 실행된 내용");
