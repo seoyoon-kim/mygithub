632338971a2683798dd8208f6cfdaf1143a37295
ArchivvonJang <76926509+ArchivvonJang@users.noreply.github.com>
2021-05-07 15:51:20 +0900

    commit

--- BeetMall/src/main/java/com/beetmall/sshj/seller/service/ProductService.java
+++ BeetMall/src/main/java/com/beetmall/sshj/seller/service/ProductService.java
@@ -10,7 +10,12 @@ public interface ProductService {
 	//검색
 	public List<ProductVO> searchList(SearchAndPageVO spvo);
 	//판매자 판매상품 목록 전체 보기
-	 public List<ProductVO> productAllSelect(String userid); 
+	public List<ProductVO> productAllSelect(String userid); 
+	//등록된 상품 갯수 ( 총 레코드 수)
+	public int totalProduct(SearchAndPageVO spvo);
+	//한페이지에 해당하는 상품 갯수 선택 ( 레코드 선택)
+	public List<ProductVO> onePageRecordSelect(SearchAndPageVO spvo); 
+	 
 	//상품 등록하기
 	public int productInsert(ProductVO vo);
 }
